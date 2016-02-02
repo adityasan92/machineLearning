b5c4883f14a944996cf3b58c5c8f2fac20550056
Aditya Sanghi <adityasan92@gmail.com>
2016-02-01 23:45:32 -0500

    Added the mlbasics stuff

---
+++ mlBasics.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+x = tf.placeholder(tf.float32, [None, 784])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
--- tensorFlow/displayingImages.py
+++ tensorFlow/displayingImages.py
@@ -1,2 +1,4 @@
-from IPython.display import Image
-Image(filename="notMNIST_large\\A\\a29ydW5pc2hpLnR0Zg==.png")
+from IPython.display import Image, display, FileLink
+local_file = FileLink("notMNIST_large/A/a29ydW5pc2hpLnR0Zg==.png")
+display(local_file)
+Image(filename="notMNIST_large/A/a29ydW5pc2hpLnR0Zg==.png")
