fe7a0bbeddb313d57637704d620c47be7b631d6b
Aditya Sanghi <adityasan92@gmail.com>
2016-02-13 15:40:08 -0500

    some changes

--- tensorFlow/dataExtraction.py
+++ tensorFlow/dataExtraction.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import tarfile
 import urllib
-from IPython.display import display, Image
+#rom IPython.display import display, Image
 from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 import cPickle as pickle
